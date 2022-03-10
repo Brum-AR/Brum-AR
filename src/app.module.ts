5befc86e5cc007ca4059bf748bd67ce906f937e4
Ifzas Nguyen <nguyen.ifzas@gmail.com>
2022-03-10 10:56:08 +0100

    removed env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ScooterModule } from './scooter/scooter.module';
 
 @Module({
-  imports: [UserModule, MongooseModule.forRoot(process.env.DATABASE_URI)],
+  imports: [UserModule, MongooseModule.forRoot(process.env.DATABASE_URI), ScooterModule],
   controllers: [AppController],
   providers: [AppService],
 })
