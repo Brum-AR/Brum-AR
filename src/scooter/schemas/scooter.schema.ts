11f042f11bc381a3a1bddba53c4d3e16075a2b21
Ifzas Nguyen <nguyen.ifzas@gmail.com>
2022-03-10 22:24:43 +0100

    addedd scooter object to test

--- src/scooter/schemas/scooter.schema.ts
+++ src/scooter/schemas/scooter.schema.ts
@@ -1,5 +1,13 @@
 import { Prop,Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
 import { Document } from 'mongoose';
+import { Accessory } from 'src/accessory/schemas/accessory.schema';
+import { Battery } from 'src/battery/schemas/battery.schema';
+import { Brake } from 'src/brake/schemas/brake.schema';
+import { Engine } from 'src/engine/schemas/engine.schema';
+import { Framework } from 'src/framework/schemas/framework.schema';
+import { ScooterCharacteristic } from 'src/scooter-characteristic/schemas/scooter-characteristic.schema';
+import { Tire } from 'src/tire/schemas/tire.schema';
 
 export type ScooterDocument = Scooter & Document;
 
@@ -16,6 +24,27 @@ export class Scooter {
 
   @Prop()
   saddle_color: string;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'ScooterCharacteristic' })
+  scooter_characteristics : ScooterCharacteristic;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Engine' })
+  engine : Engine;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Battery' })
+  battery : Battery;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Framework' })
+  framework : Framework;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Brake' })
+  brake : Brake;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Tire' })
+  tire : Tire;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Accessory' })
+  accessory : Accessory;
 
   @Prop()
   created_at: Date;
--- src/scooter/scooter.service.ts
+++ src/scooter/scooter.service.ts
@@ -1,18 +1,50 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { AccessoryService } from 'src/accessory/accessory.service';
+import { BatteryService } from 'src/battery/battery.service';
+import { BrakeService } from 'src/brake/brake.service';
+import { EngineService } from 'src/engine/engine.service';
+import { FrameworkService } from 'src/framework/framework.service';
+import { ScooterCharacteristicService } from 'src/scooter-characteristic/scooter-characteristic.service';
+import { TireService } from 'src/tire/tire.service';
 import { Scooter, ScooterDocument } from './schemas/scooter.schema';
 
 @Injectable()
 export class ScooterService {
 
-  constructor(@InjectModel(Scooter.name) private scooterModel: Model<ScooterDocument>) {}
+  constructor(@InjectModel(Scooter.name) private scooterModel: Model<ScooterDocument>,
+              private readonly scooterCharacteristicService: ScooterCharacteristicService,
+              private readonly engineService: EngineService,
+              private readonly batteryService: BatteryService,
+              private readonly frameworkService: FrameworkService,
+              private readonly brakeService: BrakeService,
+              private readonly tireService: TireService,
+              private readonly accessoryService: AccessoryService) {}
 
   async create(): Promise<Scooter> {
 
+    const scooter_characteristics = this.scooterCharacteristicService.create();
+    const engine = this.engineService.create();
+    const battery = this.batteryService.create();
+    const framework = this.frameworkService.create();
+    const brake = this.brakeService.create();
+    const tire = this.tireService.create();
+    const accessory = this.accessoryService.create();
+
     const createdScooter = new this.scooterModel(
       {
-
+        name: "scooter",
+        price: 3200,
+        color: "vert",
+        saddle_color: "noir",
+        scooter_characteristics: scooter_characteristics,
+        engine: engine,
+        battery: battery,
+        framework: framework,
+        brake: brake,
+        tire: tire,
+        accessory: accessory,
         created_at: new Date()
       }
     )
