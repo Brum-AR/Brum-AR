d74326ccf95e3b5f7f9116393d93db1327ddef03
henrimeng_predilife <henri.meng@predilife.com>
2022-03-10 12:22:55 +0100

    Add Product

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ScooterModule } from './scooter/scooter.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [MongooseModule.forRoot("mongodb+srv://root:root@cluster0.kpikx.mongodb.net/brumar?retryWrites=true&w=majority"), UserModule, ScooterModule],
+  imports: [MongooseModule.forRoot("mongodb+srv://root:root@cluster0.kpikx.mongodb.net/brumar?retryWrites=true&w=majority"), UserModule, ScooterModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
