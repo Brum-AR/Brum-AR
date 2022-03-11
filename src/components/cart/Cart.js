ec86cb27f85ef501e67f5b374fec3c32e3f56e29
henrimeng_predilife <henri.meng@predilife.com>
2022-03-11 12:03:13 +0100

    Update Panier

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from 'react';
 import './Cart.css';
 import axios from "axios";
 import {getImg} from "../common/FileUtils";
+import { useNavigate } from "react-router-dom";
+
 
 const Cart = (props) => {
     let [loading, setLoading] = useState(true);
@@ -23,6 +25,13 @@ const Cart = (props) => {
         console.log(articles)
     }
 
+    function price(item) {
+        console.log(queryStream.get('top_case'))
+        if (queryStream.get('top_case').toLowerCase() === 'oui')
+            return item.price += 64.90
+        return item.price;
+    }
+
     function postCart() {
         setLoading(true)
         const body = {
@@ -37,6 +46,7 @@ const Cart = (props) => {
             .finally(() => {
                 setArticles([])
                 setLoading(false)
+                props.history.push('/success')
             })
     }
 
@@ -74,7 +84,7 @@ const Cart = (props) => {
                 <div className='cart__articles'>
                     {
                         Array.from(articles).map(it => (
-                            <div className='article'>
+                            <div key={it._id} className='article'>
                                 <img className='article__img' src={getImg(it.color)} alt={'...'}/>
                                 <p className='article__name'>{it.name || ''}</p>
                                 <div>
@@ -82,7 +92,7 @@ const Cart = (props) => {
                                     <p className='article__option'>{'SIÈGE : ' + it.saddle_color || ''}</p>
                                     <p className='article__option'>TOP CASE : {queryStream.get('top_case') || 'Non'}</p>
                                 </div>
-                                <p className='article__price'>{it.price || ''}€</p>
+                                <p className='article__price'>{(it.price) || ''}€</p>
                             </div>
                         ))
                     }
